d729590a659f2d85b166e3d763692a5e351cab14
rfrende <rfrende@consensusgroup.net>
2020-09-01 19:49:21 -0300

    deploy things

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,21 +1,6 @@
 // vue.config.js
 module.exports = {
-    publicPath: ‘/sunFlower/’
-    }
-
-const options = {
-    transformAssetUrls: {
-        video: ['src', 'poster'],
-        source: 'src',
-        img: 'src',
-        image: 'xlink:href',
-        'b-avatar': 'src',
-        'b-img': 'src',
-        'b-img-lazy': ['src', 'blank-src'],
-        'b-card': 'img-src',
-        'b-card-img': 'src',
-        'b-card-img-lazy': ['src', 'blank-src'],
-        'b-carousel-slide': 'img-src',
-        'b-embed': 'src'
-    }
-    }
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/sun-flower/'
+    : '/'
+}
